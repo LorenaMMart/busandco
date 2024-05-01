0b1df8c0ec502ff3364c0c108bc563fb44a65ceb
LorenaMMart <lorena.martinezbis1@gmail.com>
2024-05-01 19:22:33 +0200

    metedo lineas y CORS

--- src/Controller/UsuarioController.php
+++ src/Controller/UsuarioController.php
@@ -4,18 +4,29 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\Encoder\XmlEncoder;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
+
 
 use Doctrine\ORM\EntityManagerInterface;
 
 use App\Entity\Linea;
 use App\Dto\ListLineasDto;
-use App\Seriazable\SerializableCollection;
-use App\Seriazable\SerializableObject;
+
 
 #[Route('/api', name: 'api_')]
 class UsuarioController extends AbstractController
 {
+   
+    public function __construct()
+    {
+       
+    }
+
     #[Route('/usuario', name: 'app_usuario')]
     public function index(): JsonResponse
     {
@@ -29,16 +40,18 @@ class UsuarioController extends AbstractController
     public function lineas(EntityManagerInterface $em): JsonResponse {
         $lineas = $em->getRepository(Linea::class)->findAll();
         if($lineas){
-            $dtoList = array();
+            $dtoList = [];
             foreach($lineas as $linea){
-                $dto = ListLineasDto::of($linea->getNombre(),
-                                        $linea->getDescripcion(),
-                                        $linea->getEmpresa()->getNombre(),
-                                        $linea->getTipo());
-                array_push($dtoList,new SerializableObject($dto));                        
+                $dto = [
+                        'id' => $linea->getId(),
+                        'nombre' => $linea->getNombre(),
+                        'descripcion' => $linea->getDescripcion(),
+                        'empresa' => $linea->getEmpresa()->getNombre(),
+                        'tipo' => $linea->getTipo(),
+                        ];
+                array_push($dtoList,$dto);                        
             }
-            // $jsonR = $this->jsonResponse($dtoList);
-            return $this->json($dtoList);
+            return new JsonResponse($dtoList, Response::HTTP_OK);
         }
         else{
             return $this->json(["error" => "Linea no encontrada"], 404);
