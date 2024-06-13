386464564609db95bf85700e9104b16b66b9920b
LorenaMMart <lorena.martinezbis1@gmail.com>
2024-06-13 20:42:26 +0200

    incorporar test2

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,11 +10,16 @@ use Doctrine\Persistence\ManagerRegistry;
 use App\Entity\Linea;
 use App\Entity\Sublinea;
 use App\Entity\Empresa;
-
+use App\Service\AdminService;
 
 #[Route('/api', name: 'api_')]
 class AdminController extends AbstractController
 {
+    private $adminService;
+    public function __construct(AdminService $admService)
+    {
+       $this->adminService = $admService;
+    }
 
     #[Route('/admin', name: 'app_admin', methods: 'GET')]
     public function admin(): JsonResponse
@@ -28,83 +33,33 @@ class AdminController extends AbstractController
     #[Route('/listado', name: 'app_listado', methods: 'GET')]
     public function listado(ManagerRegistry $mr) : JsonResponse
     {
-        $lineas =  $mr->getRepository(Linea::class)->findAll();
+        $result =$this->adminService->listado($mr);
+        if(isset($result['error']))
+            return $this->json(["error" => $result['error']], $result['code']);
+        else
+            return $this->json($result);
 
-        $data = [];
-        foreach($lineas as $linea){
-            if($linea->isActiva()){
-                $sublineas = $linea->getSublineas();
-                $data[] = [
-                    'id'    => $linea->getId(),
-                    'linea' => $linea->getNombre(),
-                    'descripion' => $linea->getDescripcion(),
-                    'empresa' => $linea->getEmpresa()->getNombre(),
-                    'sublineas' => $sublineas,
-                    'tipo' => $linea->getTipo()
-                ];
-            }
-        }
-        if(count($data) != 0)
-        {
-            return $this->json($data);
-        }
-        else{
-            return $this->json(["error" => "No se han encontrado lineas"], 404);
-        }
     }
 
     #[Route('/listadoEmpresas', name: 'app_listadoEmp', methods: 'GET')]
     public function listadoEmpresas(ManagerRegistry $mr) : JsonResponse
     {
-        $empresas =  $mr->getRepository(Empresa::class)->findAll();
-
-        $data = [];
-        foreach($empresas as $empresa){
-            $data[] = [
-                'id'    => $empresa->getId(),
-                'linea' => $empresa->getNombre(),
-                'direccion' => $empresa->getDireccion(),
-                'telefono' => $empresa->getTelefono(),
-                'email' => $empresa->getEmail(),
-                'web' => $empresa->getWeb()
-            ];
-            
-        }
-        if(count($data) != 0)
-        {
-            return $this->json($data);
-        }
-        else{
-            return $this->json(["error" => "No se han encontrado empresas"], 404);
-        }
+        $result =$this->adminService->listadoEmpresas($mr);
+        if(isset($result['error']))
+            return $this->json(["error" => $result['error']], $result['code']);
+        else
+            return $this->json($result);
     }
 
     #[Route('/addlinea', name: 'app_addlinea', methods: 'POST')]
-    public function addlinea(ManagerRegistry $mr, Request $request) : JsonResponse
+    public function addLinea(ManagerRegistry $mr, Request $request) : JsonResponse
     {
         try{
-            if(isset($request)) 
-            {
-                $entityManager = $mr->getManager();
-                $linea = new Linea();
-                $parameter = json_decode($request->getContent(), true);
-                $linea->setNombre($parameter['linea']);
-                $linea->setDescripcion($parameter['descripcion']);
-                //Lo que espera recibir es un Objeto empresa
-                $empresa = $mr->getRepository(Empresa::class)->find($parameter['empresa']);
-                $linea->setEmpresa($empresa);
-                //Lo que espera recibir es una Coleccion de objetos Sublinea
-                $sublinea = $mr->getRepository(Sublinea::class)->find($parameter['sublinea']);
-                $linea->addSublinea($sublinea);
-                $linea->setTipo($parameter['tipo']);
-                $entityManager->persist($linea);
-                $entityManager->flush();
-
-                return $this->json('Una nueva linea ha sido creada satisfactoriamente con id ' . $linea->getId());
-            }
-            else{
-                return $this->json('Algo ha ido mal', 400);
-            }
+            $result =$this->adminService->addLinea($mr, $request);
+            if(isset($result['error']))
+                return $this->json(["error" => $result['error']], $result['code']);
+            else
+                return $this->json($result);
         }
         catch(\Exception $ex)
         {
@@ -115,68 +70,22 @@ class AdminController extends AbstractController
     #[Route('/verLinea/{id}', name: 'app_verLinea', methods: 'GET')]
     public function verLinea(ManagerRegistry $mr, $id) : JsonResponse
     {
-       $linea = $mr->getRepository(Linea::class)->find($id);
-       if(!$linea || $linea->isActiva() == false){
-            return $this->json('No se ha encontrado la linea con id' . $id, 404);
-       }else{
-            $data = [
-                'id' => $linea->getId(),
-                'linea' => $linea->getNombre(),
-                'descripcion' => $linea->getDescripcion(),
-                'empresa' =>$linea->getEmpresa()->getNombre(),
-                'tipo' =>$linea->getTipo()
-            ];
-            return $this->json($data);
-        }
+        $result =$this->adminService->verLinea($mr, $id);
+        if(isset($result['error']))
+            return $this->json(["error" => $result['error']], $result['code']);
+        else
+            return $this->json($result);
     }
 
     #[Route('/editarLinea/{id}', name: 'app_editarLinea', methods: ['PUT' , 'PATCH'])]
     public function editarLinea(ManagerRegistry $mr, Request $request, $id) : JsonResponse
     {
         try{
-            $entityManager = $mr->getManager();
-            $linea = $entityManager->getRepository(Linea::class)->find($id);
-            
-            if(!$linea || $linea->isActiva() == false){
-                return $this->json('No se ha encontrado la linea con id' . $id, 404);
-            }
+            $result =$this->adminService->editarLinea($mr,$request,$id);
+            if(isset($result['error']))
+                return $this->json(["error" => $result['error']], $result['code']);
             else
-            {
-                $sublineas = $linea->getSublineas();
-                $parameter = json_decode($request->getContent(), true);
-                $linea->setNombre($parameter['linea']);
-                $linea->setDescripcion($parameter['descripcion']);
-                $empresa = $mr->getRepository(Empresa::class)->find($parameter['empresa']);
-                $linea->setEmpresa($empresa);
-
-                //Recorremos las sublineas recuperadas de las lineas activas, comprobamos que existen datos y que la nueva sublinea no exsite ya
-                $sublExiste = false;
-                if(count($sublineas)>0 && isset($parameter['sublinea'])){
-                    foreach($sublineas as $sublinea){
-                        if($sublinea->getNombre() == $parameter['sublinea']){
-                            $sublExiste = true;
-                        }
-                    }
-                }
-                if(!$sublExiste && isset($parameter['sublinea'])){
-                    $sublineaCrear = new Sublinea();
-                    $sublineaCrear->setNombre($parameter['sublinea']);
-                    $linea->addSublinea($sublineaCrear);
-                }    
-                
-                $linea->setTipo($parameter['tipo']);
-                $entityManager->flush();
-                $data = [
-                    'id'    => $linea->getId(),
-                    'linea' => $linea->getNombre(),
-                    'descripion' => $linea->getDescripcion(),
-                    'empresa' => $linea->getEmpresa(),
-                    'sublineas' => $linea->getSublineas(),
-                    'tipo' => $linea->getTipo(),
-                    'activa' => $linea->isActiva()
-                ];
-                return $this->json($data);
-            }
+                return $this->json($result);  
         }
         catch(\Exception $ex)
         {
@@ -187,59 +96,32 @@ class AdminController extends AbstractController
     #[Route('/borrarLinea/{id}', name: 'app_borrarLinea', methods: ['PUT' , 'PATCH'])]
     public function borrarLinea(ManagerRegistry $mr, $id) : JsonResponse
     {
-       $entityManager = $mr->getManager(); 
-       $linea = $entityManager->getRepository(Linea::class)->find($id);
-
-       if(!$linea || $linea->isActiva() == false){
-            return $this->json('No se ha encontrado la linea con id' . $id, 404);
-       }else{
-            $linea->setActiva(false);
-            $entityManager->persist($linea);
-            $entityManager->flush();
-            return $this->json('La linea con id ' . $linea->getId() . " ha sido borrada");
-        }
+        $result =$this->adminService->borrarLinea($mr,$id);
+        if(isset($result['error']))
+            return $this->json(["error" => $result['error']], $result['code']);
+        else
+            return $this->json($result);
     }
 
     #[Route('/listadoBorradas', name: 'app_listadoBorradas', methods: 'GET')]
     public function listadoBorradas(ManagerRegistry $mr) : JsonResponse
     {
-        $lineas = $mr->getRepository(Linea::class)->findAll();
-            
-        $data = [];
-        foreach($lineas as $linea){
-            if($linea->isActiva() == false){
-                $data[] = [
-                    'id'    => $linea->getId(),
-                    'linea' => $linea->getNombre(),
-                    'descripion' => $linea->getDescripcion(),
-                    'empresa' => $linea->getEmpresa()->getNombre(),
-                    'sublineas' => $linea->getSublineas(),
-                    'tipo' => $linea->getTipo(),
-                ];
-            }
-        }
-        if(count($data) != 0)
-            return $this->json($data);
-        else{
-            return $this->json(["error" => "No se han encontrado lineas borradas"], 404);
-        }
+        $result =$this->adminService->listadoBorradas($mr);
+        if(isset($result['error']))
+            return $this->json(["error" => $result['error']], $result['code']);
+        else
+            return $this->json($result);
         
     }
 
     #[Route('/recuperarLinea/{id}', name: 'app_recuperarLinea', methods: ['PUT' , 'PATCH'])]
     public function recuperarLinea(ManagerRegistry $mr, $id) : JsonResponse
     {
-       $entityManager = $mr->getManager(); 
-       $linea = $entityManager->getRepository(Linea::class)->find($id);
-
-       if(!$linea || $linea->isActiva() == true){
-            return $this->json('No se ha encontrado la linea borrada con id' . $id, 404);
-       }else{
-            $linea->setActiva(true);
-            $entityManager->persist($linea);
-            $entityManager->flush();
-            return $this->json('La linea con id ' . $linea->getId() . " ha sido recuperada");
-        }
+        $result =$this->adminService->recuperarLinea($mr, $id);
+        if(isset($result['error']))
+            return $this->json(["error" => $result['error']], $result['code']);
+        else
+            return $this->json($result);
     }
 
 }
